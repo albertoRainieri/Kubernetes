966d415ac602f8774ef0ad3682be39e0087dd28b
albertoRainieri <alberto.rainieri.lavoro@gmail.com>
2023-02-21 12:43:16 +0100

    HA K8S Cluster with Ansible

---
+++ Ansible_HA/joincluster-master.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.61.100:6443 --token nehvhy.z6qgp9wf4v8d3apu --discovery-token-ca-cert-hash sha256:162dd3c741a4a88a7e612f9cb2bbbc43beeef71fb601c3850a3cca0be3e8b640 --control-plane --certificate-key c221b132f57e345455c6899338a19a5711e292c66dbaf3ec55015ce8582f704c
---
+++ Ansible_HA/save_private_keys.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+loadbalancer1=192.168.61.51
+loadbalancer2=192.168.61.52
+kmaster1=192.168.61.101
+kmaster2=192.168.61.102
+kmaster3=192.168.61.103
+kworker1=192.168.61.201
+path=$HOME/Kubernetes/vagrant_init/Ansible_HA
+
+ssh-keygen -f "/home/alberto/.ssh/known_hosts" -R $loadbalancer1
+ssh-keygen -f "/home/alberto/.ssh/known_hosts" -R $loadbalancer2
+ssh-keygen -f "/home/alberto/.ssh/known_hosts" -R $kmaster1
+ssh-keygen -f "/home/alberto/.ssh/known_hosts" -R $kmaster2
+ssh-keygen -f "/home/alberto/.ssh/known_hosts" -R $kmaster3
+ssh-keygen -f "/home/alberto/.ssh/known_hosts" -R $kworker1
+
+eval `ssh-agent -s`
+ssh-add "$path"/.vagrant/machines/loadbalancer1/virtualbox/private_key
+ssh -o StrictHostKeyChecking=accept-new vagrant@"$loadbalancer1" 'echo hello loadbalancer1'
+sleep 1
+ssh-add "$path"/.vagrant/machines/loadbalancer2/virtualbox/private_key
+ssh -o StrictHostKeyChecking=accept-new vagrant@"$loadbalancer2" 'echo hello loadbalancer2'
+sleep 1
+ssh-add "$path"/.vagrant/machines/kmaster1/virtualbox/private_key
+ssh -o StrictHostKeyChecking=accept-new vagrant@"$kmaster1" 'echo hello kmaster1'
+sleep 1
+ssh-add "$path"/.vagrant/machines/kmaster2/virtualbox/private_key
+ssh -o StrictHostKeyChecking=accept-new vagrant@"$kmaster2" 'echo hello kmaster2'
+sleep 1
+ssh-add "$path"/.vagrant/machines/kmaster3/virtualbox/private_key
+ssh -o StrictHostKeyChecking=accept-new vagrant@"$kmaster3" 'echo hello kmaster3'
+sleep 1
+ssh-add "$path"/.vagrant/machines/kworker1/virtualbox/private_key
+ssh -o StrictHostKeyChecking=accept-new vagrant@"$kworker1" 'echo hello kworker1'
+sleep 1
+
